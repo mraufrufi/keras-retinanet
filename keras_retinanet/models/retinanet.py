63aae371d18121975730b5e5764ca828a1bbbd43
bw4sz <benweinstein2010@gmail.com>
2018-07-13 14:37:59 -0700

    didn't add it to source, again

--- keras_retinanet/models/retinanet.py
+++ keras_retinanet/models/retinanet.py
@@ -303,7 +303,7 @@ def retinanet_bbox(
     model                 = None,
     anchor_parameters     = AnchorParameters.default,
     nms                   = True,
-    nms_threshold=nms_threshold,
+    nms_threshold=None,
     class_specific_filter = True,
     name                  = 'retinanet-bbox',
     **kwargs
