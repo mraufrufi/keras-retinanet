21619e529daf9ba75fad40a55b2b6664902990fc
bw4sz <benweinstein2010@gmail.com>
2018-08-08 15:00:51 -0700

    non-max supression across tiles

--- keras_retinanet/utils/eval.py
+++ keras_retinanet/utils/eval.py
@@ -286,76 +286,13 @@ def JaccardEvaluate(
         #BGR order
         numpy_image=numpy_image[:,:,::-1].copy()
         
-        #get sliding windows
-        windows=compute_windows(numpy_image)
+        #Gather detections
+        final_boxes=predict_tile(numpy_image)
+        draw_detections(numpy_image, final_boxes[:,:4], final_boxes[:,4], final_boxes[:,5], label_to_name=generator.label_to_name,score_threshold=0.05,color=(255,0,0))  
         
-        #holder for all detections among windows within tile
-        plot_detections=[]
-        
-        #Prediction for each window
-        for window in windows:
-            raw_image=retrieve_window(numpy_image,window)
-        
-            #utilize the generator to scale?
-            image, scale = generator.resize_image(raw_image)
-            
-            # run network
-            boxes, scores, labels = model.predict_on_batch(np.expand_dims(image, axis=0))[:3]
-            
-            # correct boxes for image scale
-            boxes /= scale
-            
-            # select indices which have a score above the threshold
-            indices = np.where(scores[0, :] > score_threshold)[0]
-            
-            # select those scores
-            scores = scores[0][indices]
-            
-            # find the order with which to sort the scores
-            scores_sort = np.argsort(-scores)[:max_detections]
-            
-            # select detections
-            image_boxes      = boxes[0, indices[scores_sort], :]
-            image_scores     = scores[scores_sort]
-            image_labels     = labels[0, indices[scores_sort]]
-            image_detections = np.concatenate([image_boxes, np.expand_dims(image_scores, axis=1), np.expand_dims(image_labels, axis=1)], axis=1)
-            
-            if len(image_detections)==0:
-                print("no detections")
-                mean_IoU=0
-            else:
-                print( "%d" % (len(image_detections)))
-                    
-                #align detections to original image
-                x,y,w,h=window.getRect()
-                
-                #boxes are in form x1, y1, x2, y2
-                image_detections[:,0] = image_detections[:,0] + x 
-                image_detections[:,1] = image_detections[:,1] + y 
-                image_detections[:,2] = image_detections[:,2] + x 
-                image_detections[:,3] = image_detections[:,3] + y 
-                
-                #Collect detection across windows
-                plot_detections.append(image_detections)                
-        
-        # non-max supression among tiles
-        
-
-        
-        for window in plot_detections:
-            #Random colors visualization
-            r = lambda: random.randint(0,255)
-            color=(r(),r(),r())
-            draw_detections(numpy_image, window[:,:4], window[:,4], window[:,5], label_to_name=generator.label_to_name,score_threshold=0.4,color=color)
-        
-        cv2.imshow("image",numpy_image)
-        while True:
-            if cv2.waitKey(0)==27:
-                break
-                    
         #Save image and send it to logger
         if save_path is not None:
-            draw_detections(numpy_image, image_boxes, image_scores, image_labels, label_to_name=generator.label_to_name,score_threshold=0.4)
+            draw_detections(numpy_image, final_boxes[:,:4], final_boxes[:,4], final_boxes[:,5], label_to_name=generator.label_to_name,score_threshold=0.05)
             cv2.imwrite(os.path.join(save_path, '{}.png'.format(plot)), raw_image)
             if experiment:              
                 experiment.log_image(os.path.join(save_path, '{}.png'.format(plot)),file_name=str(i))
@@ -439,3 +376,120 @@ def compute_windows(numpy_image,pixels=400,overlap=0.05):
 def retrieve_window(numpy_image,window):
     crop=numpy_image[window.indices()]
     return(crop)
+
+
+def non_max_suppression(boxes, overlapThresh):
+    # if there are no boxes, return an empty list
+    if len(boxes) == 0:
+        return []
+
+    # if the bounding boxes integers, convert them to floats --
+    # this is important since we'll be doing a bunch of divisions
+    if boxes.dtype.kind == "i":
+        boxes = boxes.astype("float")
+
+    # initialize the list of picked indexes	
+    pick = []
+
+    # grab the coordinates of the bounding boxes
+    x1 = boxes[:,0]
+    y1 = boxes[:,1]
+    x2 = boxes[:,2]
+    y2 = boxes[:,3]
+
+    # compute the area of the bounding boxes and sort the bounding
+    # boxes by the bottom-right y-coordinate of the bounding box
+    area = (x2 - x1 + 1) * (y2 - y1 + 1)
+    idxs = np.argsort(y2)
+
+    # keep looping while some indexes still remain in the indexes
+    # list
+    while len(idxs) > 0:
+        # grab the last index in the indexes list and add the
+        # index value to the list of picked indexes
+        last = len(idxs) - 1
+        i = idxs[last]
+        pick.append(i)
+
+        # find the largest (x, y) coordinates for the start of
+        # the bounding box and the smallest (x, y) coordinates
+        # for the end of the bounding box
+        xx1 = np.maximum(x1[i], x1[idxs[:last]])
+        yy1 = np.maximum(y1[i], y1[idxs[:last]])
+        xx2 = np.minimum(x2[i], x2[idxs[:last]])
+        yy2 = np.minimum(y2[i], y2[idxs[:last]])
+
+        # compute the width and height of the bounding box
+        w = np.maximum(0, xx2 - xx1 + 1)
+        h = np.maximum(0, yy2 - yy1 + 1)
+
+        # compute the ratio of overlap
+        overlap = (w * h) / area[idxs[:last]]
+
+        # delete all indexes from the index list that have
+        idxs = np.delete(idxs, np.concatenate(([last],
+                                                       np.where(overlap > overlapThresh)[0])))
+
+    # return the indices for only the bounding boxes that were picked using the
+    # integer data type
+    return pick
+
+
+def predict_tile(numpy_image):
+    #get sliding windows
+    windows=compute_windows(numpy_image)
+    
+    #holder for all detections among windows within tile
+    plot_detections=[]
+    
+    #Prediction for each window
+    for window in windows:
+        raw_image=retrieve_window(numpy_image,window)
+    
+        #utilize the generator to scale?
+        image, scale = generator.resize_image(raw_image)
+        
+        # run network
+        boxes, scores, labels = model.predict_on_batch(np.expand_dims(image, axis=0))[:3]
+        
+        # correct boxes for image scale
+        boxes /= scale
+        
+        # select indices which have a score above the threshold
+        indices = np.where(scores[0, :] > score_threshold)[0]
+        
+        # select those scores
+        scores = scores[0][indices]
+        
+        # find the order with which to sort the scores
+        scores_sort = np.argsort(-scores)[:max_detections]
+        
+        # select detections
+        image_boxes      = boxes[0, indices[scores_sort], :]
+        image_scores     = scores[scores_sort]
+        image_labels     = labels[0, indices[scores_sort]]
+        image_detections = np.concatenate([image_boxes, np.expand_dims(image_scores, axis=1), np.expand_dims(image_labels, axis=1)], axis=1)
+        
+        if len(image_detections)==0:
+            print("no detections")
+            mean_IoU=0
+        else:
+            print( "%d" % (len(image_detections)))
+                
+            #align detections to original image
+            x,y,w,h=window.getRect()
+            
+            #boxes are in form x1, y1, x2, y2
+            image_detections[:,0] = image_detections[:,0] + x 
+            image_detections[:,1] = image_detections[:,1] + y 
+            image_detections[:,2] = image_detections[:,2] + x 
+            image_detections[:,3] = image_detections[:,3] + y 
+            
+            #Collect detection across windows
+            plot_detections.append(image_detections)                
+    
+    #Non-max supression
+    all_boxes=np.concatenate(plot_detections)
+    final_box_index=non_max_suppression(all_boxes[:,:4], overlapThresh=0.2)
+    final_boxes=all_boxes[final_box_index,:]
+    return final_boxes
